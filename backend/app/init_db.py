9f7b896db1de8a8b47c91ee98d62521d53a5fc6b
Sai Nikhil <162953542+SaiNikhil206@users.noreply.github.com>
2026-02-20 15:42:18 +0530

    modified the create_engine function in init_db.py

--- backend/app/init_db.py
+++ backend/app/init_db.py
@@ -6,7 +6,15 @@ load_dotenv()
 
 database_url = Config.CONNECTION_PG_DB
 
-engine = create_engine(database_url)
+engine = create_engine(database_url,
+        connect_args={
+            "sslmode": "require",
+            "connect_timeout": 10,
+        },
+        pool_size=20,
+        max_overflow=0,
+        pool_pre_ping=True,  
+        pool_recycle=300,)
 
 sessionLocal = sessionmaker(autoflush=False,expire_on_commit=False,bind=engine)
 
