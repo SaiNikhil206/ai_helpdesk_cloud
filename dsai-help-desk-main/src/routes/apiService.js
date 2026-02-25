cc6126320f6129ab2cb2888e43c7669b820bbd67
Nikhil <sainikhil.badathala@easystep.com>
2026-02-25 17:21:00 +0530

    fix: Update apiClient baseURL to use Process.env for better environment handling

--- dsai-help-desk-main/src/routes/apiService.js
+++ dsai-help-desk-main/src/routes/apiService.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+
+
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+    baseURL: Process.env.VITE_PUBLIC_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
